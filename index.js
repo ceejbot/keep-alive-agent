a927b7b18cd756439ac5bf7657d3a76bc13ab43e
C J Silverio <ceejceej@gmail.com>
2012-12-04 19:34:47 -0800

    Formatting changes to comply with Kelp Heavy Weaponry's fierce standards.
    The cats bite your ankles if you don't use Allman bracing.

--- index.js
+++ index.js
@@ -1,131 +1,135 @@
 var
-    http = require('http'),
-    https = require('https'),
-    util = require('util')
-    ;
+	http = require('http'),
+	https = require('https'),
+	util = require('util')
+	;
 
+//----------------------------------------------------------------------------------------
 
+function KeepAliveAgent(options)
+{
 
-function KeepAliveAgent(options) {
+	options = options || {};
+	http.Agent.call(this, options);
 
-    options = options || {};
-    http.Agent.call(this, options);
+	// Keys are host:port names, values are lists of sockets.
+	this.idleSockets = {};
 
-    // Keys are host:port names, values are lists of sockets.
-    this.idleSockets = {};
-
-    // Replace the 'free' listener set up by the default node Agent above.
-    this.removeAllListeners('free');
-    this.on('free', freeHandler.bind(this));
+	// Replace the 'free' listener set up by the default node Agent above.
+	this.removeAllListeners('free');
+	this.on('free', KeepAliveAgent.prototype.freeHandler.bind(this));
 
 }
 util.inherits(KeepAliveAgent, http.Agent);
 
-function buildNameKey(host, port, localAddress) {
-    var name = host + ':' + port;
-    if (localAddress) {
-        name += ':' + localAddress;
-    }
+function buildNameKey(host, port, localAddress)
+{
+	var name = host + ':' + port;
+	if (localAddress)
+		name += ':' + localAddress;
 
-    return name;
+	return name;
 }
 
-function freeHandler(socket, host, port, localAddress) {
+KeepAliveAgent.prototype.freeHandler = function(socket, host, port, localAddress)
+{
+	var name = buildNameKey(host, port, localAddress);
 
-    var name = buildNameKey(host, port, localAddress);
+	// If the socket is still useful, return it to the idle pool.
+	if (this.isSocketUsable(socket))
+	{
+		socket._requestCount = socket._requestCount ? socket._requestCount + 1 : 1;
 
-    // If the socket is still useful, return it to the idle pool.
-    if (this.isSocketUsable(socket)) {
-        socket._requestCount = socket._requestCount ? socket._requestCount + 1 : 1;
+		if (!this.idleSockets[name])
+			this.idleSockets[name] = [];
 
-        if (!this.idleSockets[name]) {
-            this.idleSockets[name] = [];
-        }
-        this.idleSockets[name].push(socket);
-    }
+		this.idleSockets[name].push(socket);
+	}
 
-    // If we had any pending requests for this name, send the next one off now.
-    if (this.requests[name] && this.requests[name].length) {
-        var nextRequest = this.requests[name].shift();
+	// If we had any pending requests for this name, send the next one off now.
+	if (this.requests[name] && this.requests[name].length)
+	{
+		var nextRequest = this.requests[name].shift();
 
-        if (!this.requests[name].length) {
-            delete this.requests[name];
-        }
+		if (!this.requests[name].length)
+			delete this.requests[name];
 
-        this.addRequest(nextRequest, host, port, localAddress);
-    }
+		this.addRequest(nextRequest, host, port, localAddress);
+	}
 }
 
-KeepAliveAgent.prototype.addRequest = function (request, host, port, localAddress) {
+KeepAliveAgent.prototype.addRequest = function(request, host, port, localAddress)
+{
+	var name = buildNameKey(host, port, localAddress);
 
-    var name = buildNameKey(host, port, localAddress);
-
-    var socket = this.nextIdleSocket(name);
-    if (socket) {
-        request.onSocket(socket);
-    } else {
-        // We have no idle sockets to re-use, so make a new one the usual way.
-        return http.Agent.prototype.addRequest.call(this, request, host, port, localAddress);
-    }
+	var socket = this.nextIdleSocket(name);
+	if (socket)
+		request.onSocket(socket);
+	else
+	{
+		// We have no idle sockets to re-use, so make a new one the usual way.
+		return http.Agent.prototype.addRequest.call(this, request, host, port, localAddress);
+	}
 };
 
-KeepAliveAgent.prototype.nextIdleSocket = function (name) {
+KeepAliveAgent.prototype.nextIdleSocket = function(name)
+{
+	if (!this.idleSockets[name])
+		return null;
 
-    if (!this.idleSockets[name]) {
-        return null;
-    }
+	var socket;
+	while(socket = this.idleSockets[name].shift())
+	{
+		// Check that this socket is still healthy after sitting around on the shelf.
+		if (this.isSocketUsable(socket))
+			return socket;
+	}
 
-    var socket;
-    while (socket = this.idleSockets[name].shift()) {
-        // Check that this socket is still healthy after sitting around on the shelf.
-        if (this.isSocketUsable(socket)) {
-            return socket;
-        }
-    }
-
-    // Sadly, no good sockets.
-    return null;
+	// Sadly, no good sockets.
+	return null;
 };
 
-KeepAliveAgent.prototype.isSocketUsable = function (socket) {
-    return !socket.destroyed;
+KeepAliveAgent.prototype.isSocketUsable = function(socket)
+{
+	return !socket.destroyed;
 };
 
 
-KeepAliveAgent.prototype.removeSocket = function (socket, name, host, port, localAddress) {
+KeepAliveAgent.prototype.removeSocket = function(socket, name, host, port, localAddress)
+{
+	if (this.idleSockets[name])
+	{
+		var idx = this.idleSockets[name].indexOf(socket);
+		if (idx !== -1)
+		{
+			this.idleSockets[name].splice(idx, 1);
+			if (!this.idleSockets[name].length)
+				delete this.idleSockets[name];
+		}
+	}
 
-    if (this.idleSockets[name]) {
-        var idx = this.idleSockets[name].indexOf(socket);
-        if (idx !== -1) {
-            this.idleSockets[name].splice(idx, 1);
-            if (!this.idleSockets[name].length) {
-                delete this.idleSockets[name];
-            }
-        }
-    }
-
-    http.Agent.prototype.removeSocket.call(this, socket, name, host, port, localAddress);
+	http.Agent.prototype.removeSocket.call(this, socket, name, host, port, localAddress);
 };
 
 //----------------------------------------------------------------------------------------
 
-function HTTPSKeepAliveAgent(options) {
-    KeepAliveAgent.call(this, options);
-    this.createConnection = https.globalAgent.createConnection;
+function HTTPSKeepAliveAgent(options)
+{
+	KeepAliveAgent.call(this, options);
+	this.createConnection = https.globalAgent.createConnection;
 }
 util.inherits(HTTPSKeepAliveAgent, KeepAliveAgent);
 
 HTTPSKeepAliveAgent.prototype.defaultPort = 443;
 
-HTTPSKeepAliveAgent.prototype.isSocketUsable = function (socket) {
-    // TLS sockets null out their ssl field in destroy() and
-    // do not set a destroyed flag the way normal sockets do.
-
-    return socket.pair && socket.pair.ssl;
+HTTPSKeepAliveAgent.prototype.isSocketUsable = function(socket)
+{
+	// TLS sockets null out their ssl field in destroy() and
+	// do not set a destroyed flag the way normal sockets do.
+	return socket.pair && socket.pair.ssl;
 };
 
-
-
+//----------------------------------------------------------------------------------------
 
 module.exports = KeepAliveAgent;
 KeepAliveAgent.Secure = HTTPSKeepAliveAgent;
